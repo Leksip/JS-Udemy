439288e185241cc283cd03df60598bdab40638f5
Leksip <Nickita.rom@gmail.com>
2022-06-01 15:24:36 +0300

    Проходим первую практику на Udemy

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-let number = 5;
-const leftBorderWidth=1;
+'use strict'
 
-console.log(10%4);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+   const personalMovieDB = {
+     count: numberOfFilms,
+     movies: {},
+     actors: {},
+     genres: [],
+     privat: false
+
+};
+   const a = prompt('Один из последних просмотренных фильмов?', ''),
+       b = prompt('На сколько оцените его?', ''),
+       c= prompt('Один из последних просмотренных фильмов?', ''),
+       d=prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB)
